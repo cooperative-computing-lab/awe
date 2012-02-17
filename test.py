6798f79c56985c724c2fd802d3b134f211d8a189
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-02-17 13:41:16 -0500

    port Jesus' awe input data

--- test.py
+++ test.py
@@ -7,26 +7,34 @@ import numpy as np
 
 awe.io.TRACE = False
 
-cfg      = awe.workqueue.Config()
-cfg.execute('test.exe2')
+cfg = awe.workqueue.Config()
+cfg.execute('testinput/execute-task.sh')
+cfg.cache('testinput/protomol.conf')
+cfg.cache('testinput/topol.tpr')
+cfg.cache('testinput/with-env')
+cfg.cache('testinput/env.sh')
+cfg.cache('testinput/Gens.lh5')
+cfg.cache('testinput/AtomIndices.dat')
+cfg.cache('testinput/state0.pdb')
 
-pdb     = mdtools.prody.parsePDB('topology.pdb')
-walkers = awe.aweclasses.WalkerGroup( count    = 2,
-                                      topology = pdb )
+nwalkers = 910
+walkers  = awe.aweclasses.WalkerGroup( count    = nwalkers,
+                                      topology = mdtools.prody.parsePDB('testinput/state0.pdb'))
 
-w       = awe.aweclasses.Walker(      coords = pdb.getCoords(),
-                                      weight = np.random.random(),
-                                      color  = 0,
-                                      cell   = 0 )
-walkers.add(w)
+for i in xrange(nwalkers):
+    pdb    = mdtools.prody.parsePDB('teststartpdbs/state%d.pdb'  % i)
+    weight = float(open(      'teststartpdbs/weight%d.txt' % i).read().strip())
+    color  = 0
+    cell   = i
+    w      = awe.aweclasses.Walker(coords = pdb.getCoords(),
+                                   weight = weight,
+                                   color  = color,
+                                   cell   = cell
+                                   )
+    walkers.add(w)
 
-w       = awe.aweclasses.Walker(      coords = pdb.getCoords(),
-                                      weight = np.random.random(),
-                                      color  = 0,
-                                      cell   = 1 )
-walkers.add(w)
 
-resample = awe.resample.Simple(2)
+resample = awe.resample.Simple(20)
 adaptive = awe.aweclasses.AWE( wqconfig   = cfg,
                                walkers    = walkers,
                                iterations = 2,
