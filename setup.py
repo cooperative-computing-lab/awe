73a6d36f2682be05f2524b4680f9089ad007d7ac
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-02-20 17:52:26 -0500

    added distutils setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+
+setup( author       = "Badi' Abdul-Wahid",
+       author_email = 'abdulwahidc@gmail.com',
+       url          = 'https://bitbucket.org/badi/awe',
+       name         = 'awe',
+       packages     = ['awe'],
+       platforms    = ['Linux', 'Mac OS X'],
+       description  = 'Adaptive Weighted Ensemble method using WorkQueue'
+       )
