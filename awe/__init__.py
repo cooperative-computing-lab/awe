15dd1769319a86f453af9404ecb40fbc263844ce
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-02-20 17:55:22 -0500

    expose common classes at toplevel import

--- awe/__init__.py
+++ awe/__init__.py
@@ -1,5 +1,10 @@
 
 from io import trace, log
 from util import typecheck, typecheckfn
+
 import workqueue, aweclasses, io, resample, stats
+
+from aweclasses import Walker, WalkerGroup, AWE
+from workqueue import Config
+
 
