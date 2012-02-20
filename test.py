f29e786ae0ff404af63779262bd1dbd44edd0adf
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-02-19 23:50:29 -0500

    save

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ import awe
 import mdtools
 
 import numpy as np
+import os
 
 awe.io.TRACE = False
 mdtools.prody.setVerbosity('warning')
 
 cfg = awe.workqueue.Config()
+cfg.fastabort = 4
 cfg.execute('testinput/execute-task.sh')
 cfg.cache('testinput/protomol.conf')
 cfg.cache('testinput/topol.tpr')
@@ -19,28 +21,32 @@ cfg.cache('testinput/AtomIndices.dat')
 cfg.cache('testinput/state0.pdb')
 
 
-nwalkers = 20
-nstates = 100
-walkers  = awe.aweclasses.WalkerGroup(count    = nwalkers,
+nwalkers = 10
+nstates  = 100
+walkers  = awe.aweclasses.WalkerGroup(count    = nwalkers * nstates,
                                       topology = mdtools.prody.parsePDB('testinput/state0.pdb'))
 
-for i in xrange(nwalkers):
-    pdb    = mdtools.prody.parsePDB('teststartpdbs/state%d.pdb'  % i)
-    weight = float(open(            'teststartpdbs/weight%d.txt' % i).read().strip())
-    color  = 0
-    cell   = i % nstates
-    w      = awe.aweclasses.Walker(coords = pdb.getCoords(),
-                                   weight = weight,
-                                   color  = color,
-                                   cell   = cell
-                                   )
-    walkers.add(w)
+srcdir = '/afs/crc.nd.edu/user/i/izaguirr/Public/ala2/faw-protomol/PDBs'
+for i in xrange(nstates):
+    weight = np.random.random()
+    for j in xrange(nwalkers):
+
+        pdbpath = os.path.join(srcdir, 'State%d-%d.pdb' % (i, j))
+        pdb     = mdtools.prody.parsePDB(pdbpath)
+        color   = 0
+        cell    = i
+        w       = awe.aweclasses.Walker(coords = pdb.getCoords(),
+                                        weight = weight,
+                                        color  = color,
+                                        cell   = cell
+                                        )
+        walkers.add(w)
 
 
-resample = awe.resample.SimpleWeightsPlotter(nwalkers, plotfile='/tmp/test.png')
+resample = awe.resample.OneColor_PlotCellRMSD(nwalkers, ref='testinput/state0.pdb', plotfile='/tmp/test.png')
 adaptive = awe.aweclasses.AWE( wqconfig   = cfg,
                                walkers    = walkers,
-                               iterations = 3,
+                               iterations = 10,
                                resample   = resample)
 
 adaptive.run()
