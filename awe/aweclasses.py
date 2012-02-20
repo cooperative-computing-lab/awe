a43e1692f38498156d33f1d8e1f69363b9afd6ab
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-02-20 11:58:49 -0500

    awe.aweclasses.Walker: default color is 0

--- awe/aweclasses.py
+++ awe/aweclasses.py
@@ -10,7 +10,7 @@ import time
 
 class Walker(object):
 
-    def __init__(self, start=None, end=None, weight=0., color=None, cell=None, wid=-1):
+    def __init__(self, start=None, end=None, weight=0., color=0, cell=None, wid=-1):
 
         assert not (start is None and end is None)
 
