d7d73c87454464ec2fd5634c7769bc78db6e7e79
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-02-20 17:14:54 -0500

    improve typecheck: handle functions and class methods differently

--- awe/util.py
+++ awe/util.py
@@ -1,22 +1,57 @@
 
+import traceback
 
-class typecheck(object):
+class TypeException (Exception): pass
 
-    def __init__(self, *args, **kws):
-        self.args = args
+class _typecheck(object):
+
+    def __init__(self, method=True):
+        self.method = method
+
+    def check(self, *args, **kws):
+        if self.method:
+            self.args = [None] + list(args)
+        else:
+            self.args = args
         self.kws  = kws
 
-    def typecheck(self, value, expected, name=''):
+        return self
+
+    def typecheck(self, value, expected, name='', arg=-1):
         typ = type(value)
-        if typ is not expected: raise TypeError, '%s expected %s but got %s' % (name or 'param', expected, typ)
+        if typ is not expected:
+             raise TypeException, '%s expected: %s, but got: %s' % (name or 'param %s' % arg, expected, typ)
 
     def __call__(self, fn):
         def wrapped(*args, **kws):
-            for v, t in zip(args, self.args):
-                self.typecheck(v, t)
-            for n, e in self.kws.iteritems():
-                if n in kws:
-                    self.typecheck(kws[n], e, n)
+ 
+            try:
+                i = -1
+                for v, t in zip(args, self.args):
+                    i += 1
+                    if self.method: continue
+                    self.typecheck(v, t, arg=i)
+                del i
+
+                for n, e in self.kws.iteritems():
+                    if n in kws:
+                        self.typecheck(kws[n], e, name=n)
+            except TypeException, ex:
+                stack = traceback.extract_stack()
+                stack = ''.join(traceback.format_list(stack[:-1]))
+                stack = '\n\t'.join(('\t' + stack).split('\n'))
+                raise TypeException, '%s:\n\n%s\n\t%s' % (fn, stack, ex)
 
             return fn(*args,**kws)
         return wrapped
+
+def typecheck(*args, **kws):
+    tc = _typecheck(method=True)
+    tc.check(*args, **kws)
+    return tc
+
+
+def typecheckfn(*args, **kws):
+    tc = _typecheck(method=False)
+    tc.check(*args, **kws)
+    return tc
