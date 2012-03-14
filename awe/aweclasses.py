b3d9b5e3ffe3e37a727ac77a7a41d2ed73482815
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-03-14 18:28:22 -0400

    add setter for Walker.color

--- awe/aweclasses.py
+++ awe/aweclasses.py
@@ -92,6 +92,9 @@ class Walker(object):
     @property
     def color(self):      return self._color
 
+    @color.setter
+    def color(self, c):   self._color = c
+
     @property
     def weight(self):     return self._weight
 
