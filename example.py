ce1c96a8dec2b41c15f8d354c697dc43fa20a924
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-03-14 18:31:48 -0400

    updated example to use new api

--- example.py
+++ example.py
@@ -14,13 +14,14 @@ cfg.restarts = 0
 cfg.execute('test.exe')
 
 # cfg.execute('testinput/execute-task.sh')
-# cfg.cache('testinput/protomol.conf')
-# cfg.cache('testinput/topol.tpr')
-# cfg.cache('testinput/with-env')
-# cfg.cache('testinput/env.sh')
-# cfg.cache('testinput/Gens.lh5')
-# cfg.cache('testinput/AtomIndices.dat')
-# cfg.cache('testinput/state0.pdb')
+
+cfg.cache('testinput/protomol.conf')
+cfg.cache('testinput/topol.tpr')
+cfg.cache('testinput/with-env')
+cfg.cache('testinput/env.sh')
+cfg.cache('testinput/Gens.lh5')
+cfg.cache('testinput/AtomIndices.dat')
+cfg.cache('testinput/state0.pdb')
 
 
 iterations = 3
@@ -28,27 +29,30 @@ nwalkers   = 2
 nstates    = 100
 
 system = awe.System(topology = awe.PDB('testinput/state0.pdb'))
+partition = awe.SinkStates()
 
 print 'Loading cells and walkers'
 srcdir = '/afs/crc.nd.edu/user/i/izaguirr/Public/ala2/faw-protomol/PDBs'
 for i in xrange(nstates):
 
-    if i < nstates/2: core = 0
-    else            : core = 1
+    if i < nstates/2: color = 0
+    else            : color = 1
 
-    cell = awe.Cell(i, core=core)
+    partition.add(color, i)
+
+    cell = awe.Cell(i)
     system.add_cell(cell)
 
     for j in xrange(nwalkers):
 
         pdbpath = os.path.join(srcdir, 'State%d-%d.pdb' % (i, j))
         pdb     = awe.PDB(pdbpath)
-        w       = awe.aweclasses.Walker(start=pdb.coords, assignment=i, color=core, weight=np.random.random())
+        w       = awe.aweclasses.Walker(start=pdb.coords, assignment=i, color=color, weight=np.random.random())
         system.add_walker(w)
 
 
 
-resample = awe.resample.MultiColor(nwalkers)
+resample = awe.resample.MultiColor(nwalkers, partition)
 # resample = awe.resample.SaveWeights(resample)
 adaptive = awe.AWE( wqconfig   = cfg,
                     system     = system,
