fef1776e41d5676e426cb8969e5e84ad16dd01af
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2013-02-17 15:27:59 -0500

    Dinesh: 1. Added a main loop.  2. Added command-line arguments to set different parameters. 3. Removed print statements that served

--- example.py
+++ example.py
@@ -3,77 +3,127 @@
 import awe
 
 import numpy as np
+import sys
+import getopt
 import os
+import getpass
 
-cfg           = awe.workqueue.Config()
-cfg.name      = 'test-awe'
-cfg.fastabort = 3
-cfg.restarts  = float('inf')
-cfg.maxreps   = 50
-cfg.debug     = 'all'
-
-cfg.execute('testinput/execute-task.sh')
-
-cfg.cache('awesetup/binaries/$OS-$ARCH/pdb2gmx')
-cfg.cache('awesetup/binaries/$OS-$ARCH/grompp')
-cfg.cache('awesetup/binaries/$OS-$ARCH/mdrun')
-cfg.cache('awesetup/binaries/$OS-$ARCH/assign')
-
-cfg.cache('awesetup/gmxtopologies')
-cfg.cache('testinput/sim.mdp')
-cfg.cache('testinput/env.sh')
-cfg.cache('testinput/cells.dat')
-cfg.cache('testinput/CellIndices.dat')
-cfg.cache('testinput/StructureIndices.dat')
-cfg.cache('testinput/state0.pdb')
-
-
+#-----Simulation Default Values-----
 iterations = 5
 nwalkers   = 4
 nstates    = 100
+restarts   = float('inf')
+maxreps    = 50
 
-weights   = np.random.random((nstates,nwalkers))
-weights  /= np.sum(weights.flatten())
+#-----WQ Default Values-----
+wq_fast_abort_multiplier = -1.0
+wq_proj_name = None 
+wq_debug_flag = None
 
-system    = awe.System(topology = awe.PDB('testinput/state0.pdb'))
+#-----Main Program------
+if __name__ == "__main__":
 
-partition = awe.SinkStates()
-partition.add(0, *range(0,50))
-partition.add(1, *range(50,100))
+	help_str = "Usage: %s" % sys.argv[0]
+	help_str += "-i		-	specify the number of iterations\n"
+	help_str += "-w		-	specify the number of walkers\n"
+	help_str += "-s		-	specify the number of states\n"
+	help_str += "-r		-	specify the number of restarts allowed\n"
+	help_str = "-n		-	specify a project name for Work Queue to use\n"
+	help_str += "-f		-	specify the Work Queue fast abort multipler\n"
+	help_str += "-d		-	print Work Queue debug messages \n"
+	help_str += "-h		-	help"
+
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "d:f:i:n:r:s:w:h", ["help"])
+	except getopt.GetoptError, err:
+		print str(err) 
+		print usage_str
+		sys.exit(1)
+
+	#Parse command line arguments.
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			print help_str
+			sys.exit(0)
+		elif o in ("-n"):
+			wq_proj_name = a
+		elif o == "-i":
+			iterations = int(a)
+		elif o in ("-w"):
+			nwalkers = int(a)
+		elif o in ("-s"):
+			nstates = int(a) 
+		elif o in ("-f"):
+			wq_fast_abort_multiplier = float(a) 
+		elif o in ("-r"):
+			restarts = int(a) 
+		elif o in ("-d"):
+			wq_debug_flag = a 
 
 
-print 'Loading cells and walkers'
-srcdir = 'awesetup/pdbs/ala'
-for i in xrange(nstates):
+	cfg           = awe.workqueue.Config()
+	cfg.fastabort = wq_fast_abort_multiplier 
+	cfg.restarts  = restarts 
+	cfg.maxreps   = maxreps
+	cfg.name      = wq_proj_name 
+	
+	if wq_debug_flag:
+		cfg.debug     =	wq_debug_flag 
 
-    if i < nstates / 3:
-        cell = awe.Cell(i, core=0)
-    elif i > 2 * nstates / 3:
-        cell = awe.Cell(i, core=1)
-    else:
-        cell = awe.Cell(i)
+	cfg.execute('testinput/execute-task.sh')
 
-    color = partition.color(cell)
-    system.add_cell(cell)
+	cfg.cache('awesetup/binaries/$OS-$ARCH/pdb2gmx')
+	cfg.cache('awesetup/binaries/$OS-$ARCH/grompp')
+	cfg.cache('awesetup/binaries/$OS-$ARCH/mdrun')
+	cfg.cache('awesetup/binaries/$OS-$ARCH/assign')
+
+	cfg.cache('awesetup/gmxtopologies')
+	cfg.cache('testinput/sim.mdp')
+	cfg.cache('testinput/env.sh')
+	cfg.cache('testinput/cells.dat')
+	cfg.cache('testinput/CellIndices.dat')
+	cfg.cache('testinput/StructureIndices.dat')
+	cfg.cache('testinput/state0.pdb')
+
+	weights   = np.random.random((nstates,nwalkers))
+	weights  /= np.sum(weights.flatten())
+
+	system    = awe.System(topology = awe.PDB('testinput/state0.pdb'))
+
+	partition = awe.SinkStates()
+	partition.add(0, *range(0,50))
+	partition.add(1, *range(50,100))
+
+	srcdir = 'awesetup/pdbs/ala'
+	for i in xrange(nstates):
+
+	    if i < nstates / 3:
+		cell = awe.Cell(i, core=0)
+	    elif i > 2 * nstates / 3:
+		cell = awe.Cell(i, core=1)
+	    else:
+		cell = awe.Cell(i)
+
+	    color = partition.color(cell)
+	    system.add_cell(cell)
 
 
-    for j in xrange(nwalkers):
+	    for j in xrange(nwalkers):
 
-        pdbpath = os.path.join(srcdir, 'State%d-%d.pdb' % (i, j))
-        pdb     = awe.PDB(pdbpath)
-        w       = awe.Walker(start=pdb.coords, assignment=i, color=color, weight=weights[i,j], cellid=cell.id)
-        system.add_walker(w)
+		pdbpath = os.path.join(srcdir, 'State%d-%d.pdb' % (i, j))
+		pdb     = awe.PDB(pdbpath)
+		w       = awe.Walker(start=pdb.coords, assignment=i, color=color, weight=weights[i,j], cellid=cell.id)
+		system.add_walker(w)
 
+	multicolor = awe.resample.MultiColor(nwalkers, partition)
+	resample   = awe.resample.SaveWeights(multicolor)
+	adaptive   = awe.AWE( wqconfig   = cfg,
+			      system     = system,
+			      iterations = iterations,
+			      resample   = resample,
+			      checkpointfreq = 1)
 
+	adaptive.run()
 
-multicolor = awe.resample.MultiColor(nwalkers, partition)
-resample   = awe.resample.SaveWeights(multicolor)
-adaptive   = awe.AWE( wqconfig   = cfg,
-                      system     = system,
-                      iterations = iterations,
-                      resample   = resample,
-                      checkpointfreq = 1)
-
-adaptive.run()
-
-print 'Run time:', awe.time.time(), 's'
+	print 'Run time:', awe.time.time(), 's'
+	sys.exit(0)
