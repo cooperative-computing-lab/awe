976a266c2f28be15a0d4e98cfd1769ccdecfaf99
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2013-02-22 13:51:06 -0500

    move scripts directory to executables (since cassign is a binary, not script)

--- setup.py
+++ setup.py
@@ -8,13 +8,21 @@ See the file COPYING for details.
 
 from distutils.core import setup
 
+import os.path
+
+
+def get_executables():
+    import glob
+    return glob.glob(os.path.join('executables', '*'))
+
+
 
 setup( author       = "Badi' Abdul-Wahid",
        author_email = 'abdulwahidc@gmail.com',
        url          = 'https://bitbucket.org/badi/awe',
        name         = 'awe',
        packages     = ['awe', 'trax'],
-       scripts      = ['scripts/awe-verify', 'scripts/awe-prepare-gromacs','scripts/awe-rama-ala.py','scripts/awe-flux.py'],
+       scripts      = get_executables(),
        platforms    = ['Linux', 'Mac OS X'],
        description  = 'Adaptive Weighted Ensemble method using WorkQueue'
        )
