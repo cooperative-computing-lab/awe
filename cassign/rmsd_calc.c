7d7e2e2b07e0ed95b26751f3175c99f8769778b7
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2013-04-02 11:15:46 -0400

    remove theobald rmsd computation (dead code)

--- cassign/rmsd_calc.c
+++ cassign/rmsd_calc.c
@@ -3,80 +3,6 @@
 
 
 
-theodata* theodata_alloc () {
-  return (theodata*) malloc (sizeof(theodata));
-}
-
-exit_t theodata_init (const size_t nreal, const size_t ndim, theodata** theo) {
-  const size_t npad = 4 + nreal - nreal % 4;
-
-  *theo = theodata_alloc ();
-  (*theo)->nreal = nreal;
-  (*theo)->npad  = npad;
-  (*theo)->ndim  = ndim;
-  (*theo)->coords = (float*) calloc (ndim * npad, sizeof(float));
-
-  return exitOK;
-}
-
-void theodata_printf (const theodata* theo) {
-  printf ("<theodata: nreal = %lu npad = %lu ndim = %lu G = %.3f>",
-	  theo->nreal, theo->npad, theo->ndim, theo->g);
-}
-
-void theodata_printf_all (const theodata* theo) {
-  theodata_printf (theo);
-  printf ("{\n");
-  for (int r=0; r<theo->ndim; r++) {
-    printf ("%3s[", " ");
-    for (int c=0; c<theo->npad; c++) {
-      printf ("%.3f, ", theodata_get (theo, r, c));
-    }
-    printf ("]\n");
-  }
-  printf ("}\n");
-}
-
-exit_t prepare_data (const gsl_matrix* mat, theodata** theo) {
-  size_t
-    Rs = mat->size1,
-    Cs = mat->size2;
-
-  gsl_matrix* mat2 = gsl_matrix_calloc (Rs, Cs);
-  theodata_init (Rs, Cs, theo);
-
-
-  gsl_matrix_memcpy (mat2, mat);
-  center_structure (mat2);
-
-  const double G = calculate_theo_g (mat2);
-  (*theo)->g = G;
-
-  for (int r=0; r<Rs; r++) {
-    for (int c=0; c<Cs; c++) {
-      const float v = (float) gsl_matrix_get (mat2, r, c);
-      theodata_set ((*theo), c, r, v);
-    }}
-
-  gsl_matrix_free (mat2);
-
-  return exitOK;
-}
-
-size_t theodata_ix (const theodata* theo, const size_t dim, const size_t coord) {
-  return dim * theo->npad + coord;
-}
-
-exit_t theodata_set (theodata* theo, const size_t dim, const size_t coord, const float val) {
-  const size_t ix = theodata_ix (theo, dim, coord);
-  theo->coords[ix] = val;
-  return exitOK;
-}
-
-float theodata_get (const theodata* theo, const size_t dim, const size_t coord) {
-  const size_t ix = theodata_ix (theo, dim, coord);
-  return theo->coords[ix];
-}
 
 exit_t center_structure (gsl_matrix* mat) {
 
@@ -100,47 +26,8 @@ exit_t center_structure (gsl_matrix* mat) {
   return exitOK;
 }
 
-double calculate_theo_g (const gsl_matrix* mat) {
-
-  double G = 0;
-  for (int d=0; d<mat->size2; d++) {
-    const gsl_vector_const_view col = gsl_matrix_const_column (mat, d);
-    double dot = 0;
-    gsl_blas_ddot (&col.vector, &col.vector, &dot);
-    G += dot;
-  }
-
-    /* for (int i=0; i<col.vector.size; i++) { */
-    /*   double s = gsl_vector_get (&col.vector, i); */
-    /*   s *= s; */
-
-    /*   // sum of squares */
-    /*   ssqrs += s; */
-    /* }} */
-
-  return G; // ssqrs;
-}
-
-double theo_rmsd (const theodata* theo1, const theodata* theo2) {
-
-  assert (theo1->nreal	== theo2->nreal);
-  assert (theo1->npad	== theo2->npad);
-  assert (theo1->ndim	== theo2->ndim);
-
-  const double msd = ls_rmsd2_aligned_T_g (theo1->nreal, theo1->npad, theo1->npad, theo1->coords, theo2->coords, theo1->g, theo2->g);
-  return sqrt (msd);
-}
-
 
 double compute_rmsd (const gsl_matrix* m1, const gsl_matrix* m2) {
-  /* theodata *theo1, *theo2; */
-
-  /* prepare_data (m1, &theo1); */
-  /* prepare_data (m2, &theo2); */
-
-  /* theodata_printf_all (theo1); */
-  /* theodata_printf_all (theo2); */
-
   return kabsch_rmsd (m1, m2);
 }
 
--- cassign/rmsd_calc.h
+++ cassign/rmsd_calc.h
@@ -6,8 +6,6 @@
 #include "celldata.h"
 #include "gsl_util.h"
 
-#include "theobald_rmsd.h"
-
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
@@ -16,42 +14,6 @@
 
 #include <math.h>
 
-typedef struct {
-  size_t nreal, npad, ndim;
-  double g;
-  float* coords;
-} theodata;
-
-
-theodata* theodata_alloc ();
-
-exit_t theodata_init (const size_t nreal, const size_t ndim, theodata** theo);
-
-size_t theodata_ix (const theodata* theo, const size_t dim, const size_t coord);
-
-exit_t theodata_set (theodata* theo, const size_t dim, const size_t coord, const float val);
-
-float theodata_get (const theodata* theo, const size_t dim, const size_t coord);
-
-void theodata_printf (const theodata* theo);
-
-void theodata_printf_all (const theodata* theo);
-
-
-/* Prepare a structure for rmsd computation using Theobald metric
-     mat: IN: the 2-rank matrix of coordinates
-     theo: OUT: the prepared data
-*/
-exit_t prepare_data (const gsl_matrix* mat, theodata** theo);
-
-
-
-/* Center the structure *in place*
-   - mat: IN, OUT: the 2-rank matrix of coordinates
-*/
-exit_t center_structure (gsl_matrix* mat);
-
-exit_t flatten (const gsl_matrix* mat, gsl_vector* vec);
 
 double compute_rmsd (const gsl_matrix* m1, const gsl_matrix* m2);
 
@@ -85,11 +47,6 @@ double kabsch_rmsd (const gsl_matrix *m1, const gsl_matrix *m2);
 
  */
 exit_t kabsch_function (const gsl_matrix *P, const gsl_matrix *Q, gsl_matrix **U, gsl_matrix **r, double *rmsd);
-
-/* Compute the G value for input into TheoRMSD */
-double calculate_theo_g (const gsl_matrix* mat);
-
-double theo_rmsd (const theodata* theo1, const theodata* theo2);
 
 
 #endif
