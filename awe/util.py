591f63daaa688878d139492ab12a62b0eebf25e2
Jeff K <zerothehero33@gmail.com>
2015-07-02 14:27:51 -0400

    Commmented util._typecheck

--- awe/util.py
+++ awe/util.py
@@ -14,11 +14,54 @@ class TypeException (Exception): pass
 
 class _typecheck(object):
 
+    """
+    awe.util._typecheck
+
+    Utility for checking that the types of parameters passed to functions are
+    of the correct type.
+
+    Fields:
+        method - the function/method to check
+        args   - arguments to the function/method
+        kws    - keyword arguments to the function/method
+    """
+
     def __init__(self, method=True):
+
+        """
+        awe.util._typecheck.__init__
+
+        Initialize a new _typecheck instance.
+
+        Parameters:
+            method - the function/method to verify parameters for 
+        
+        Returns:
+            None 
+        """
+
         self.method = method
 
     def check(self, *args, **kws):
+
+        """
+        awe.util._typecheck.__init__
+
+        Assign the arguments and keyword arguments to their instance fields,
+        including accounting for the fact that Python sends a reference to the
+        object instance when calling a method.
+
+        Parameters:
+            args - arguments to the function/method to check
+            kws  - keyword arguments to the function/method to check
+
+        Returns:
+            The calling _typecheck instance
+        """
+
         if self.method:
+            # If calling a method, prepend None to account for the fact that
+            # an object reference is passed along with the argument list.
             self.args = [None] + list(args)
         else:
             self.args = args
@@ -27,16 +70,45 @@ class _typecheck(object):
         return self
 
     def typecheck(self, value, expected, name='', arg=-1):
+
+        """
+        awe.util._typecheck.typecheck
+
+        Check that a value's type matches the expected type for that value.
+
+        Parameters: 
+            value    - the value to check
+            expected - the expected type of the value
+            name     - the name of the variable/parameter containing the value
+            arg      - the position of the parameter in an argument list
+        """
+
         typ = type(value)
         if typ is not expected:
              raise TypeException, '%s expected: %s, but got: %s' % (name or 'param %s' % arg, expected, typ)
 
     def __call__(self, fn):
+        
+        """
+        awe.util._typecheck.__call__
+
+        The action taken when a _typecheck instance is called. This allows it
+        to wrap functions and have them called like normal while performing the
+        _typecheck functionality behind-the-scenes.
+
+        Parameters:
+            fn - the function to check and run
+
+        Returns:
+            The function wrapped in a function to do typechecking
+        """
+
         def wrapped(*args, **kws):
- 
-            try:
+
+            try:    # Iterate over the argument list and check type
                 i = -1
                 for v, t in zip(args, self.args):
+                    # if it is a method, skip this part
                     i += 1
                     if self.method: continue
                     self.typecheck(v, t, arg=i)
@@ -53,6 +125,8 @@ class _typecheck(object):
 
             return fn(*args,**kws)
 
+        # Ensure that wrapping the function does not overwrite its
+        # visible identity (i.e. it should display the same name and docstring)
         wrapped.func_name = fn.func_name
         wrapped.func_doc = fn.func_doc
         return wrapped
